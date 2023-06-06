d5c36d1c4d967fc486c3d0d74b45e29485942e49
jDudek <jakub.dudek94@gmail.com>
2023-06-06 21:34:30 +0200

    application.properties - aspect logging flag added | LoggingAspect - ConditionalOnProperty annotation added

--- src/main/java/com/demo/githubapi/aspect/LoggingAspect.java
+++ src/main/java/com/demo/githubapi/aspect/LoggingAspect.java
@@ -3,11 +3,13 @@ package com.demo.githubapi.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
+@ConditionalOnProperty(value = "logging.aspect.enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingAspect {
 
     @Pointcut("execution(* com.demo.githubapi.service.GitHubService.*(..))")
@@ -15,19 +17,24 @@ public class LoggingAspect {
     }
 
     @Before("gitHubServiceMethods()")
-    public void logBefore(JoinPoint joinPoint) {
+    public void logBeforeGitHubServiceMethods(JoinPoint joinPoint) {
         log.debug("Executing method: {}", joinPoint.getSignature().toShortString());
     }
 
     @AfterThrowing(pointcut = "gitHubServiceMethods()", throwing = "exception")
-    public void logAfterThrowing(JoinPoint joinPoint, Exception exception) {
+    public void logAfterGitHubServiceMethodsThrowing(JoinPoint joinPoint, Exception exception) {
         log.debug("Exception thrown in method: {}", joinPoint.getSignature().toShortString());
         log.debug("Exception message and cause: {}", exception.getMessage() + " | " + exception.getCause());
     }
 
     @AfterReturning(pointcut = "gitHubServiceMethods()", returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+    public void logAfterGitHubServiceMethodsReturning(JoinPoint joinPoint, Object result) {
         log.debug("Method executed successfully: {}", joinPoint.getSignature().toShortString());
         log.debug("Result: {}", result);
+    }
+
+    @After("execution(* com.demo.githubapi.config.GitHubClientConfig.gitHub(..))")
+    public void logAfterGitHubClientBeanCreation() {
+        log.debug("GitHubClient configured successfully");
     }
 }
