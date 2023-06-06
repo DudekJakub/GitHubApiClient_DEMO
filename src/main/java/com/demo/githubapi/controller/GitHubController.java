43a6c98779521c1632faf4d94fd2c69951c50552
jDudek <jakub.dudek94@gmail.com>
2023-06-06 21:36:21 +0200

    GitHubController - GetMapping url updated + method name as well to follow Richardson model

--- src/main/java/com/demo/githubapi/controller/GitHubController.java
+++ src/main/java/com/demo/githubapi/controller/GitHubController.java
@@ -3,7 +3,7 @@ package com.demo.githubapi.controller;
 import com.demo.githubapi.model.dto.RepositoryResponseDto;
 import com.demo.githubapi.service.GitHubService;
 import org.kohsuke.github.GHFileNotFoundException;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +18,13 @@ public class GitHubController {
 
     private final GitHubService gitHubService;
 
+    @Autowired
     public GitHubController(final GitHubService gitHubService) {
         this.gitHubService = gitHubService;
     }
 
-    @GetMapping(value = "/repositories/user/{login}", produces = "!application/xml")
-    public ResponseEntity<List<RepositoryResponseDto>> getUserNotForkedRepositoriesByUserLogin(final @PathVariable String login) throws GHFileNotFoundException {
+    @GetMapping(value = "/user/{login}/repositories", produces = "!application/xml")
+    public ResponseEntity<List<RepositoryResponseDto>> userNotForkedRepositoriesByUserLogin(final @PathVariable String login) throws GHFileNotFoundException {
         return ResponseEntity.ok(gitHubService.getUserNotForkedRepositoriesByUserLogin(login));
     }
 }
